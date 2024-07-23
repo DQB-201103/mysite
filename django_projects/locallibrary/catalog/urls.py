3b6ed1f14a9c0ecd6a27e11c5daf6d776ef88a0c
Bảo Đỗ <109888400+kuokubao@users.noreply.github.com>
2024-07-23 11:45:04 +0700

    Update Part3

--- django_projects/locallibrary/catalog/urls.py
+++ django_projects/locallibrary/catalog/urls.py
@@ -1,6 +1,12 @@
+# catalog/urls.py
+
 from django.urls import path
 from . import views
-urlpatterns = [
-    
-]
 
+app_name = 'catalog'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='book-list'),
+    path('book/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
+]
