0f6cec6a882a644c67965a0fd44127c85f165af5
Bảo Đỗ <109888400+kuokubao@users.noreply.github.com>
2024-07-23 11:45:04 +0700

    Part9: Working with form

--- django_projects/locallibrary/catalog/urls.py
+++ django_projects/locallibrary/catalog/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
 from . import views
+from .views import index
+
 urlpatterns = [
-    
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='book-list'),
+    path('book/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+    path('authors/', views.AuthorListView.as_view(), name='author-list'),
+    path('author/<int:pk>/', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('all-borrowed/', views.AllBorrowedView.as_view(), name='all-borrowed'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
 ]
+
+
 
